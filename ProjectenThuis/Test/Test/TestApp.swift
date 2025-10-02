d53940657900123ea29ca98bd9b7ed1419889174
nathalie macco <nathaliemacco@MacBook-Air-van-nathalie.local>
2025-10-02 14:42:09 +0200

    test

---
+++ ProjectenThuis/Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by nathalie macco on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
