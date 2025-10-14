4f9b7ae84bea504ce87260cf4e00acecf048de84
Mathisse Pattyn <mathissepattyn@iMacE.local>
2025-10-14 12:27:49 +0200

    labo2

--- labo2/labo2_2/labo2_2/ContentView.swift
+++ labo2/labo2_2/labo2_2/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        TabView{
+            Tab("departure", systemImage: "airplane.departure"){
+                
+            }
+            Tab("Info", systemImage: "info.bubble.fill"){
+                
+            }
+            Tab("Arrival", systemImage: "airplane.arrival"){
+                
+            }
         }
-        .padding()
     }
 }
 
