b8d314dd8df6ba96e8d9beb7a3e322a616a5056d
Mathisse Pattyn <mathissepattyn@iMacE.local>
2025-12-03 10:45:44 +0100

    7

---
+++ labo7/labo7/Views/ArtworkView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkView.swift
+//  labo7
+//
+//  Created by Mathisse Pattyn on 03/12/2025.
+//
+
+import SwiftUI
+
+struct ArtworkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtworkView()
+}
---
+++ labo7/labo7/Views/GalleryDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryDetailView.swift
+//  labo7
+//
+//  Created by Mathisse Pattyn on 03/12/2025.
+//
+
+import SwiftUI
+
+struct GalleryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryDetailView()
+}
